4e3c53f017c734241e54eaad35be006e6cf7b662
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-09-10 17:41:28 -0300

    Implementação da classe membro

---
+++ src/Membro.java
@@ -0,0 +1,47 @@
+package Biblioteca;
+
+import java.util.ArrayList;
+
+public class Membro {
+
+	private int id;
+	private String nome;
+	public ArrayList<Livros> livrosEmprestados = new ArrayList<Livros>();
+	
+	
+	//Construtor Membro
+	public Membro(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void membroPegaEmprestado(Livros livro) {
+        if (livro != null && !livro.isEmprestado()) {
+            livro.emprestar(id);
+            livrosEmprestados.add(livro);
+        }
+    }
+
+	public void membroRetornaLivro(Livros livro) {
+        if (livro != null && livro.isEmprestado()) {
+            livro.devolver();
+            livrosEmprestados.remove(livro);
+        }
+    }
+	
+	
+}
