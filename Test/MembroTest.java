cc7d2cb4402ad0325b5b07a6a152542790c1ba8c
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-09-10 17:41:10 -0300

    teste da classe membro

---
+++ Test/MembroTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Biblioteca.Livros;
+import Biblioteca.Membro;
+
+class MembroTest {
+	
+	Membro membro= new Membro(1, "Test");
+	
+
+	@Test
+	void testCriarMembro() {
+		assertEquals(1, membro.getId());
+		 assertEquals("Test", membro.getNome());
+	}
+	
+	 @Test
+	    public void testMembroPegaEmprestado() {
+		   Livros livro = new Livros( 1 , "Test", "Test2");
+	        
+	        membro.membroPegaEmprestado(livro);
+	        
+	        assertTrue(livro.isEmprestado());
+	    }
+	
+	@Test
+        public void testMembroRetornaLivro() {
+             Livros livro = new Livros( 1 , "Test", "Test2");
+
+            membro.membroPegaEmprestado(livro);
+            membro.membroRetornaLivro(livro);
+
+            assertFalse(livro.isEmprestado());
+        }
+
+}
